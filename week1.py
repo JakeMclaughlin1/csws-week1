2ef3ff3e0790ca47b161f5f2dc6163ba8bed9c11
“Jake <J.McLaughlin1@2022.ljmu.ac.uk>
2023-01-20 12:52:25 +0000

    Practicing pyhthon

--- week1.py
+++ week1.py
@@ -1,2 +1,37 @@
 for x in range(10) :
     print("Hello World!" + str(x))
+    
+    message = "Hello World"
+    print (message)
+    
+    message = "Yo Phython"
+    print (message)
+    
+    name = "Jake McLaughlin"
+    print (name.upper)
+    print (name.lower)
+    
+    #Creating variables for function
+    first_name = "Jake"
+    last_name = "McLaughlin"
+    #Combining said variables
+    full_name = f"{first_name} {last_name}"
+    message = (f"Hello, {full_name.title()}")
+    #Printing the code 
+    print(message)
+    
+    message = "One of Phythons strengths is it's diverse community"
+    print (message)
+    
+    message = 'One of phythons strengths is its diverse community'
+    print (message)
+    
+    name = "Eric"
+    print (f"Hello, {name} would you like to learn some python")
+    
+    print ('Albert Einstein once said'" I have no special talent I am only passionately curious")
+    
+    print(5*3)
+    #A program that displays my favourite number
+    fav_num = 7
+    print (f"My favourite number is {fav_num}")
